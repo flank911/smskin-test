4773ece20ebcc60e5a1987edfe03165e7b353128
Yuriy Alonso <ygalonso@yandex.ru>
2020-12-16 12:58:48 +0500

    first commit

---
+++ app/Article.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $guarded = [];
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+}
